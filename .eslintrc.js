3d76bf9a4daa61c1e2f9daa097ec93d1715ed8de
hy4k <mithun@fets.in>
2025-03-13 23:57:00 +0530

    Configured Git to use LF and fixed line endings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
   },
   extends: [
     "eslint:recommended",
@@ -13,7 +13,8 @@ module.exports = {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
+    "linebreak-style": "off"  // Disabling linebreak-style rule
   },
   overrides: [
     {
